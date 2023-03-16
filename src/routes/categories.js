8b809527c681b4328ffa1e8d2627a2ec3525b79e
victor lunes <victorllunes@gmail.com>
2023-03-16 15:21:54 -0300

    feat: put method for categories

--- src/routes/categories.js
+++ src/routes/categories.js
@@ -12,7 +12,7 @@ const findOne = (id) => {
 
 router.get('/', (req, res) => {
     try {
-        db.query("SELECT * FROM categories", (error, response) =>{
+        db.query("SELECT * FROM categories ORDER BY id ASC", (error, response) =>{
         if (error) {
             return res.status(500).json(error)
         }
@@ -33,9 +33,9 @@ router.post('/', (req, res) =>{
         const values = [name]
 
         db.query(text, values, (error, response) => {
-            if(error){
-                return res.status(500).json(error)
-            }
+        if(error){
+            return res.status(500).json(error)
+        }
         return res.status(200).json(response.rows)
     })
     } catch (error) {
@@ -53,20 +53,53 @@ router.delete('/:id', async (req, res) =>{
         }
         const query = findOne(id)
         const category = await db.query(query)
-            if(!category.rows[0]) {
-                return res.status(400).json({ error: "Category not found" })
-            }
+        if(!category.rows[0]) {
+            return res.status(400).json({ error: "Category not found" })
+        }
 
         const text = 'DELETE FROM categories WHERE id=$1 RETURNING *'
         const values = [Number(id)]
         const deleteResponse = await db.query(text, values)
-            if(!category.rows[0]) {
-                return res.status(400).json({ error: "Category not deleted" })
-            }
-        return res.status(200).json(deleteResponse.rows)
+        if(!deleteResponse.rows[0]) {
+            return res.status(400).json({ error: "Category not deleted" })
+        }
+        return res.status(200).json(deleteResponse.rows[0])
 
     } catch (error) {
         return res.status(500).json(error)
+    }
+})
+
+router.put('/:id', async (req, res) => {
+    try{
+        const { id } = req.params;
+        const { name } = req.body
+
+        if (!id) {
+            return res.status(400).json({ error: "Param id is mandatory" })
+        }
+
+        if(name.length < 3){
+            return res.status(400).json({ error: "Name should have more than 3 characteres"})
+        }
+
+        const query = findOne(id)
+        const category = await db.query(query)
+        if(!category.rows[0]) {
+            return res.status(400).json({ error: "Category not found" })
+        }
+
+        const text = "UPDATE categories SET name=$1 WHERE id=$2 RETURNING *"
+        const values = [name, Number(id)]
+
+        const updateResponse = await db.query(text, values)
+        if (!updateResponse.rows[0]){
+            return res.status(400).json({ error : "Category not updated"})
+        }
+        return res.status(200).json(updateResponse.rows[0])
+
+    }catch(error){
+        res.status(500).json(error)
     }
 })
 
